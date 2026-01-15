1d5ce286e6c4ac9df325b9ab0837b0f4a3961e78
Danielle Balilla <ellebalilla9+1@gmail.com>
2026-01-15 22:46:44 +0800

    draft
    Added shuffling of game cards for Rock Paper Scissors.

--- RPS.java
+++ RPS.java
@@ -1,1 +1,14 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+public class RockPaperScissors {
+    public static void main(String[] args) {
+        String[] cards = {"Rock", "Paper", "Scissors", "Joker", "Rock", "Paper", "Scissors", "Paper", "Scissors"};
+        List<String> list = Arrays.asList(cards);
+
+        Collections.shuffle(list);
+        System.out.println(list);
+
+    }
+}
